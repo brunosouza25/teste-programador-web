e039221793d6a9ac234d6cf90a12b7409c9ebf80
Bruno Souza <bruno.souza@intouchbiz.com>
2024-01-07 23:50:45 -0300

    New column status in cart table

--- src/Entity/Cart.php
+++ src/Entity/Cart.php
@@ -13,8 +13,23 @@ class Cart
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column]
+    private ?bool $status = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function isStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(bool $status): static
+    {
+        $this->status = $status;
+
+        return $this;
     }
 }
