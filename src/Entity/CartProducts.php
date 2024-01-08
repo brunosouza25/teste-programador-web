c7162679862b9d35836381ad1b8371f4f247d14e
Bruno Souza <bruno.souza@intouchbiz.com>
2024-01-07 23:00:50 -0300

    Added cart and cart_products table Ran migrations

---
+++ src/Entity/CartProducts.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CartProductsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CartProductsRepository::class)]
+class CartProducts
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $product_id = null;
+
+    #[ORM\Column]
+    private ?int $quantity = null;
+
+    #[ORM\Column]
+    private ?int $cart_id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProductId(): ?int
+    {
+        return $this->product_id;
+    }
+
+    public function setProductId(int $product_id): static
+    {
+        $this->product_id = $product_id;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): static
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getCartId(): ?int
+    {
+        return $this->cart_id;
+    }
+
+    public function setCartId(int $cart_id): static
+    {
+        $this->cart_id = $cart_id;
+
+        return $this;
+    }
+}
